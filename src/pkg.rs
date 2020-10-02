ff2c93286cc3645d600bdb4cfe5b7c2780a51f8b
heiwa4126 <heiwa4126@gmail.com>
2020-10-02 06:59:28 +0000

    update

--- src/pkg.rs
+++ src/pkg.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use serde::Deserialize;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::path::Path;
 
@@ -11,18 +12,48 @@ pub struct Pkg {
     arch: String,
 }
 
+impl Pkg {
+    pub fn cmp(&self, b: &Pkg) -> Ordering {
+        let rc = self.name.cmp(&b.name);
+        if rc != Ordering::Equal {
+            return rc;
+        }
+        let rc = self.version.cmp(&b.version);
+        if rc != Ordering::Equal {
+            return rc;
+        }
+        return self.arch.cmp(&b.arch);
+    }
+}
+
+pub fn build_pkg(name: &str, ver: &str, arch: &str) -> Pkg {
+    return Pkg {
+        name: name.to_string(),
+        version: ver.to_string(),
+        arch: arch.to_string(),
+    };
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MainPkg {
-    name: String,
-    desc: String,
+    pub name: String,
+    pub desc: String,
 }
 
-// #[derive(Debug, Deserialize)]
-// #[serde(rename_all = "camelCase")]
-// pub struct I686Pkg {
-//     name: String,
-// }
+pub const ARCH_X86: &str = "x86_64";
+pub const ARCH_I686: &str = "i686";
+pub const ARCH_NOARCH: &str = "noarch";
+
+pub fn ver_arch(verarch: &str) -> Option<(&str, &str)> {
+    for arch in &[ARCH_X86, ARCH_I686, ARCH_NOARCH] {
+        if verarch.ends_with(&(".".to_string() + &arch.to_string())) {
+            let ver = &verarch[..(verarch.len() - arch.len() - 1)];
+            return Some((ver, arch));
+        }
+    }
+    return None;
+}
 
 pub fn read_main_json(jsonfile: &str) -> Result<Vec<MainPkg>> {
     let json_file_path = Path::new(jsonfile);
@@ -37,4 +68,20 @@ pub fn read_i686_json(jsonfile: &str) -> Result<Vec<String>> {
     let file = File::open(json_file_path)?;
     let pkgs: Vec<String> = serde_json::from_reader(file)?;
     return Ok(pkgs);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_verarch() {
+        let s = "8-2.el6.noarch";
+        if let Some((ver, arch)) = ver_arch(s) {
+            assert_eq!(ver, "8-2.el6");
+            assert_eq!(arch, "noarch");
+        } else {
+            panic!("ERROR")
+        }
+    }
 }
--- src/readjson7.rs
+++ src/readjson7.rs
@@ -1,18 +1,36 @@
 use crate::pkg;
 use anyhow::Result;
+use std::collections::HashSet;
 
 fn main2i686(jsonfile: &str) -> String {
     return jsonfile[..jsonfile.len() - 5].to_string() + "_i686.json";
 }
 
 pub fn read_json7(jsonfile: &str) -> Result<Vec<pkg::Pkg>> {
+    let s1 = pkg::read_main_json(jsonfile)?;
+
+    let i686 = pkg::read_i686_json(&main2i686(jsonfile))?;
+    // let i686: HashSet<_> = i686.iter().cloned().collect();
+    let i686: HashSet<_> = i686.iter().collect();
+
+    // println!("{:#?}", s1);
+    // println!("{:#?}", i686);
+
     let mut pkgs: Vec<pkg::Pkg> = Vec::new();
 
-    // let s1 = pkg::read_main_json(jsonfile)?;
-    // println!("{:?}", s1);
-    let s2 = pkg::read_i686_json(&main2i686(jsonfile))?;
-    println!("{:?}", s2);
-
+    for l in s1 {
+        if let Some(i) = l.desc.find(" from ") {
+            if let Some((ver, arch)) = pkg::ver_arch(&l.desc[..i]) {
+                let pkg = pkg::build_pkg(&l.name, ver, arch);
+                pkgs.push(pkg);
+                if arch == pkg::ARCH_X86 && i686.contains(&l.name) {
+                    let pkg = pkg::build_pkg(&l.name, ver, pkg::ARCH_I686);
+                    pkgs.push(pkg);
+                }
+            }
+        }
+    }
+    pkgs.sort_by(|a, b| a.cmp(b));
     return Ok(pkgs);
 }
 
@@ -22,8 +40,9 @@ mod tests {
 
     #[test]
     fn test_json71() {
-        let _ = read_json7("./test/7/web02.json").expect("ERROR");
-        assert_eq!(1 + 1, 2);
+        // just read sample data
+        let rc = read_json7("./test/7/web02.json").expect("ERROR");
+        println!("{:#?}", rc);
     }
     #[test]
     fn test_i686() {
